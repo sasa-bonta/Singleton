abb15f68f4fd1f168ae227f39f55150dcd5feef9
sasa-bonta <bonta2000@mail.ru>
2021-02-01 00:34:59 +0200

    Singleton 1 ex 3 implementari vrode merge bine

---
+++ src/com/company/Database.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private String dbURL;
+    private String user;
+    private String pass;
+    private Connection connection;
+
+    public Database(String dbURL, String user, String pass) {
+        this.dbURL = dbURL;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public String getDbURL() {
+        return dbURL;
+    }
+
+    public void setDbURL(String dbURL) {
+        this.dbURL = dbURL;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public Connection getConnection() {
+        try {
+            this.connection = DriverManager.getConnection(dbURL, user, pass);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Definition:
+ * The singleton pattern is a design pattern that restricts
+ * the instantiation of a class to one object.
+ * Let’s see various design options for implementing
+ * such a class. If you have a good handle on static class
+ * variables and access modifiers this should not be a difficult task.
+ *
+ * Sources:
+ * https://www.geeksforgeeks.org/singleton-design-pattern/#:~:text=The%20singleton%20pattern%20is%20one,every%20object%20may%20be%20costly.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
